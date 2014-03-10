d515cb7ac7da3d918b01f59abdcb395b38bc9b40
Valyn Tucker <valyn.tucker@city1st.com>
2014-03-10 07:16:08 -0600

    add inline links for admin pages

--- apps/CONTrack/backend/views/layouts/main.php
+++ apps/CONTrack/backend/views/layouts/main.php
@@ -34,6 +34,8 @@ AppAsset::register($this);
 			]);
 			$menuItems = [
 				['label' => 'Home', 'url' => ['/site/index']],
+				['label' => 'Main', 'url' => ['../']],
+				['label' => 'Admin Pages', 'url' => ['/entity']],
 			];
 			if (Yii::$app->user->isGuest) {
 				$menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
@@ -51,16 +53,42 @@ AppAsset::register($this);
 			NavBar::end();
 		?>
 
-		<div class="container">
+		<div class="container-fluid">
 		<?= Breadcrumbs::widget([
 			'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
 		]) ?>
+        <!--CHANGED VJT 3/10/14: add side links div sidenav-->
+        <div >
+        	<ul style="list-style-type:none;margin:0 auto;">
+            	<li style="display:inline;padding-right:15px;"><a href="/admin/account/">Accounts</a></li>
+            	<li style="display:inline;padding-right:15px;"><a href="/admin/address/">Addresses</a></li>
+                <li style="display:inline;padding-right:15px;"><a href="/admin/building/">Buildings</a></li>
+            	<!--<li style="display:inline;padding-right:15px;"><a href="/admin/campaign/">Campaigns</a></li>-->
+                <li style="display:inline;padding-right:15px;"><a href="/admin/classification/">Classifications</a></li>
+            	<li style="display:inline;padding-right:15px;"><a href="/admin/code/">Codes</a></li>
+                <li style="display:inline;padding-right:15px;"><a href="/admin/contact/">Contacts</a></li>
+                <!-- <li style="display:inline;padding-right:15px;"><a href="/admin/correspondence/">Correspondence</a></li>
+               <li style="display:inline;padding-right:15px;"><a href="/admin/documents/">Documents</a></li>-->
+                <li style="display:inline;padding-right:15px;"><a href="/admin/email/">Email</a></li>
+                <li style="display:inline;padding-right:15px;"><a href="/admin/entity/">Entities</a></li>
+                <li style="display:inline;padding-right:15px;"><a href="/admin/phone/">Phone</a></li>
+                <li style="display:inline;padding-right:15px;"><a href="/admin/picklist/">Picklist</a></li>
+                <!--<li style="display:inline;padding-right:15px;"><a href="/admin/projects/">Projects</a></li>-->
+                <li style="display:inline;padding-right:15px;"><a href="/admin/project-team/">Project-Teams</a></li>
+                <li style="display:inline;padding-right:15px;"><a href="/admin/rate/">Rates</a></li>
+                <li style="display:inline;padding-right:15px;"><a href="/admin/relationship/">Relationships</a></li>
+                <li style="display:inline;padding-right:15px;"><a href="/admin/role/">Roles</a></li>
+                <li style="display:inline;padding-right:15px;"><a href="/admin/task/">Tasks</a></li>              
+            </ul>
+
+        
+        </div><!-- end sidenav -->
 		<?= $content ?>
 		</div>
 	</div>
 
 	<footer class="footer">
-		<div class="container">
+		<div class="container-fluid">
       <? //CHANGED MVW 02/26/24: From My Company ?>
   		<p class="pull-left">&copy; Dutch Web Development <?= date('Y') ?></p>
       <? //CHANGED MVW 02/26/24: Delete powered() ?>
